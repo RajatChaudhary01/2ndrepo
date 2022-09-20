8fe916ae4420bd0db8c3bf349ec09cfbaeb427a6
RajatChaudhary01 <rajatchoudhary9667@gmail.com>
2022-09-20 18:57:07 +0530

    2nd

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <html>
+      <body>
+      Hey there its just a practice assingnment for deplyment
+      </body>
+    </html>
+    
+  )
+}
